8b3350b0bd68bfb7438be7a2df4cdc2abec51a38
Ivan <ivan.andrau@gmail.com>
2025-10-30 02:08:53 +0100

    initial commit

---
+++ muscledia-ai-service/src/main/java/com/muscledia/muscledia_ai_service/MusclediaAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.muscledia.muscledia_ai_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusclediaAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusclediaAiServiceApplication.class, args);
+	}
+
+}
---
+++ muscledia-ai-service/src/test/java/com/muscledia/muscledia_ai_service/MusclediaAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscledia.muscledia_ai_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusclediaAiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
